1e2e206ff9a0580605c9c806262e7fa1086168e0
hajni77 <frecskahajni@gmail.com>
2024-06-20 16:21:45 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .' 
+
+def get_requirements(file: str) -> List[str]:
+    '''
+    Get requirements from file
+    '''
+    requirements=[]
+    with open(file) as f:
+        requirements = f.read().splitlines() #because slash also read
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+name ='mlproject',
+version='0.0.1',
+author='Hajni Frecska',
+author_email='frecskahajni@gmail.com',
+package=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
